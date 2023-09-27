ae62a7349a71a77f9628d64d4ea6084f38303350
Marie Weiel <marie.weiel@kit.edu>
2023-09-27 09:23:24 +0200

    cleaned up package init file

--- triehard/__init__.py
+++ triehard/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# The __init__.py file is used to mark a directory as a Python package.
+# It is used to initialize the package when it is imported.
+# It can contain code that will be executed when the package is imported,
+# as well as function definitions and variable assignments.
 from pkg_resources import DistributionNotFound, get_distribution
 
 try:
-    # Change here if project is renamed and does not equal the package name
+    # Change if project is renamed and name does not equal the package name.
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
