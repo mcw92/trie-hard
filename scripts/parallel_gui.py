07e39feabe491925a9e83cec1060b6d6d289f7a2
Marie Weiel <marie.weiel@kit.edu>
2023-09-29 10:05:56 +0200

    added support for freezing in multiprocessing to enable building exe with pyinstaller

--- scripts/parallel_gui.py
+++ scripts/parallel_gui.py
@@ -1,8 +1,11 @@
 import tkinter as tk  # Standard library for creating graphical user interfaces in Python
+import multiprocessing
+
 from triehard import TrieStatic
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     parallel = True
     root = tk.Tk()  # Create a GUI window.
     app = TrieStatic(root, parallel)
--- scripts/parallel_live.py
+++ scripts/parallel_live.py
@@ -1,8 +1,13 @@
+#!/Users/marieweiel/Projects/vector/vector-venv/bin/python
+
 import tkinter as tk  # Standard library for creating graphical user interfaces in Python
+import multiprocessing
+
 from triehard import TrieLive
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     parallel = True
     root = tk.Tk()  # Create a GUI window.
     app = TrieLive(root, parallel)
